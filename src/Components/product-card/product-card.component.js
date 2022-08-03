f3a3c1166f609e148f5ec25504ae11966ce0d08d
Lilisegovia <liliosegovia@gmail.com>
2022-08-03 15:03:03 -0400

    Added Shopping cart and Checkout

--- src/Components/product-card/product-card.component.js
+++ src/Components/product-card/product-card.component.js
@@ -7,6 +7,10 @@ import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext)
+  
+  const addProductToCart = () => addItemToCart(product)
+
   return (
   <div className="product-card-container">
     <img src={imageUrl} alt={`${name}`} />
@@ -14,7 +18,7 @@ const ProductCard = ({ product }) => {
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
-    <Button buttonType="inverted">Add to card</Button>
+    <Button buttonType="inverted" onClick={addProductToCart}>Add to card</Button>
   </div>
   )
 };
